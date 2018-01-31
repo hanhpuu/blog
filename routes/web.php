f035575c43098378f5ca8c7b5c27f324b91dffb9
hanhpuu <35247425+hanhpuu@users.noreply.github.com>
2018-01-31 10:07:58 +0700

    Models & Database Migrations

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,5 @@
 Route::get('/','PagesController@index');
 Route::get('/about','PagesController@about');
 Route::get('/services','PagesController@services');
+
+Route::resource('post', 'PostsController');
