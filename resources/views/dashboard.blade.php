800decb26b224ce63a2339f639b63510430e28f5
hanhpuu <35247425+hanhpuu@users.noreply.github.com>
2018-02-01 10:27:18 +0700

    Model Relationships

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,14 +9,37 @@
 
                 <div class="panel-body">
                     @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
+		    <div class="alert alert-success">
+			{{ session('status') }}
+		    </div>
                     @endif
 
                     You are logged in!<hr>
 		    <a href="/posts/create" class='btn btn-primary'>Create Post</a>
 		    <h3>Your blog post</h3>
+		    @if((count($posts))>0)
+		    <table class="table table-striped">
+			<tr>
+			    <td>Title</td>
+			    <td></td>
+			    <td></td>
+			</tr>
+			@foreach($posts as $post)
+			<tr>
+			    <td> {{$post->title}} </td>
+			    <td><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a></td>
+			    <td>
+				{!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method'=>'POST', 'class'=>'pull-right'])!!}
+				{{Form::hidden('_method','DELETE')}}
+				{{Form::submit('Delete',['class'=>'btn btn-danger'])}}
+				{!!Form::close() !!}
+			    </td>
+			</tr>
+			@endforeach
+		    </table>
+		    @else
+		    <p>You have no posts</p>
+		    @endif
                 </div>
             </div>
         </div>
